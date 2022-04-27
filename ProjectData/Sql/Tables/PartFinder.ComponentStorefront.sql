5b3dbb44e29868315468fd2ee091e4ddf4f2296a
Noah Sandford <nsandford64@ksu.edu>
2022-04-27 10:42:31 -0500

    added updated on column

--- ProjectData/Sql/Tables/PartFinder.ComponentStorefront.sql
+++ ProjectData/Sql/Tables/PartFinder.ComponentStorefront.sql
@@ -4,6 +4,7 @@ CREATE TABLE PartFinder.ComponentStorefront
 	StoreID INT NOT NULL FOREIGN KEY REFERENCES PartFinder.Storefront(StoreID),
 	Price DECIMAL(5,2) NOT NULL,
 	InStock BIT NOT NULL,
+	UpdatedOn DATETIME DEFAULT(SYSDATETIMEOFFSET())
 
 	PRIMARY KEY(ComponentID, StoreID)
 );
